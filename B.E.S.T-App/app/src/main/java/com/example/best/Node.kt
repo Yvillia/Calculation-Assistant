1e3406986bc4978ff9848f1735acb1ff8b2fb231
noahrr2 <noahrr2@illinois.edu>
2019-11-19 14:12:23 -0600

    feat: more android init

--- B.E.S.T-App/app/src/main/java/com/example/best/Node.kt
+++ B.E.S.T-App/app/src/main/java/com/example/best/Node.kt
@@ -2,6 +2,7 @@ package com.example.best
 
 open class Node (setNext: Node?, setValue: Any?, setLast: Node?) {
 
+    // Preliminary Task for Kotlin Practice
     var next: Node? = setNext
     var value: Any? = setValue
     var previous: Node? = setLast
