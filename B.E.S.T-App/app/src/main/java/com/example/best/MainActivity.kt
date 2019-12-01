f5cc04fcd5754e1d6dda11fec8eafdbe617d56e5
noahrr2 <noahrr2@illinois.edu>
2019-12-01 12:58:20 -0600

    feat: setup main UI, with basic listener functionality

--- B.E.S.T-App/app/src/main/java/com/example/best/MainActivity.kt
+++ B.E.S.T-App/app/src/main/java/com/example/best/MainActivity.kt
@@ -2,21 +2,41 @@ package com.example.best
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var items : Array<Any> = arrayOf(10, 20, 15, 30, 45)
-        var list : LinkedList = object : LinkedList (items) {}
-        Toast.makeText(this, list.toPrint(), Toast.LENGTH_LONG).show()
-        Log.d(list.toPrint(), list.toPrint())
-        var but = findViewById<Button>(R.id.button2).setText(list.toPrint())
+        val submit: Button = findViewById(R.id.submission)
+        val input: TextView = findViewById(R.id.expression)
+
+        submit.setOnClickListener {
+            val passed: String = input.text.toString()
+            inputHandler(passed)
+        }
+    }
+
+    /**
+     * Backend to Handle User Input
+     * Function Will Be Completed Later
+     *
+     */
+    fun inputHandler(input: String) : String {
+        return input
+    }
+
+    /**
+     * Once again a Backend Function
+     * Will Return an Output and Place it Inside TextView in activity_return.xml
+     */
+    fun toReturn(output: String) {
 
     }
+
+    /**
+     * Create a Return Button to Allow Users to Start Over Again
+     */
 
 }
